1b39e8e6420e839b4e56831a6e781cd9477d1b16
Hadeel-Ayman <haddelayman4@gmail.com>
2023-09-20 15:59:01 +0300

    update

--- Models/chapterModel.js
+++ Models/chapterModel.js
@@ -4,8 +4,12 @@ const chapterSchema = new mongoose.Schema({
     title: { type: String, required: [true, "title is required"], trim: true, unique: true },
     numberOfFiles: { type: Number, default: 0, },
     files: { type: [String], default: [] },
-    subject: { type: mongoose.Types.ObjectId, ref: 'Subject', required: [true, "subject id is required"] } 
-})
- 
+    desc: { type: String, trim: true, required: [true, "description id is required"] },
+    subject: { type: mongoose.Types.ObjectId, ref: 'Subject', required: [true, "subject id is required"] },
+},
+    {
+        timestamps: true
+    })
+
 const Chapter = mongoose.model("Chapter", chapterSchema)
 module.exports = Chapter;
